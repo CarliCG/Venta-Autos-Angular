aa295f56fdd656cc6c297911940e78d2b7f81af8
CarliCG <carlicam22@gmail.com>
2024-03-21 18:18:19 -0500

    Funcionalidad boton de guardar nuevo vehiculo (Funciona)

--- src/app/paginas/PagVehiculoRegistro/PagVehiculoRegistro.component.ts
+++ src/app/paginas/PagVehiculoRegistro/PagVehiculoRegistro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from '../../utilitarios/Modelos/Vehiculo';
+import { VehiculoService } from '../../servicios/Vehiculo.service';
 
 @Component({
   selector: 'app-PagVehiculoRegistro',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagVehiculoRegistroComponent implements OnInit {
 
-  constructor() { }
+  vehiculo: Vehiculo
+
+  constructor( 
+    private vehiculoServicio: VehiculoService
+    )
+    { 
+    this.vehiculo = {
+      codigo: '',
+      marca: '',
+      color: '',
+      modelo: '',
+      kilometraje:0,
+      precio:0,
+      foto:null,
+      ano:0,
+      calificacion:0
+    }
+  }
 
   ngOnInit() {
   }
 
+  guardar(){
+this.vehiculoServicio.addvehiculo(this.vehiculo);
+console.log('Grabado con exito');
+  }
 }
